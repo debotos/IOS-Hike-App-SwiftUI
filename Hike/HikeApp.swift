f5de00e321e654fda36381a5fa9afd7d0cfb3715
Debotos Das <debotosdas@gmail.com>
2023-07-27 09:02:58 +0600

    Initial Commit

---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by Personal on 27/7/23.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
